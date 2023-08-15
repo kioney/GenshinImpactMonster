a927deda7150f27a31d881e6abfd22bf1ed745d4
kioney <2016270771@qq.com>
2023-08-16 04:59:37 +0800

    1

--- src/main/java/com/kioney/genshinimpact/GenshinImpact.java
+++ src/main/java/com/kioney/genshinimpact/GenshinImpact.java
@@ -19,9 +19,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GenshinImpact.MOD_ID)
 public class GenshinImpact {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "genshin_impact";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     public GenshinImpact() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
